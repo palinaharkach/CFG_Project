0c2bf26262fe7b9ca2621597b693f560aa597e80
NatalyBMota <interactivedesign@yahoo.com>
2023-07-26 17:39:09 -0500

    Separated the Lookup Medical Info section content into a different HTML file, so that this content would display at the top of the page even when a user selected a different option from the drop down select form field. Added a bottom margin to the BMI calculator section of the index.html file, so that even after removing the Lookup Info content from that file, users are still able to scroll down enough to hide the 'List Your Allergies' button from view.

---
+++ js/app.js
@@ -0,0 +1,118 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
+const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those that haven't been confirmed.";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul); // Inserting the div before the ul
+
+filterCheckBox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = ul.children; // children provides a reference to a collection of all an element's children
+  if (isChecked) {
+    for (let i = 0; i < lis.length; i += 1) {
+      let li = lis[i];
+      if (li.className === 'responded') {
+        li.style.display = ''; // This will allow it to pickup its previous style.
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for (let i = 0; i < lis.length; i += 1) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+});
+
+
+function createLI(text) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
+  
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+  /* The above line of code will cancel the browser'd default submit behavior involving reloading the page and sending data to a server. */
+  
+  const text = input.value;
+  input.value = '';
+  li = createLI(text);  
+  ul.appendChild(li);
+});
+
+ul.addEventListener('change', (e) => {
+  console.log(e.target.checked);
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+  
+  if (checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = '';
+  }
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.textContent === 'remove') {
+      
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') {
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      /* What does the line of code above do?
+         "We can use the span to place the new input element into the DOM using insertBefore()." 
+         "We want to place the new input element first, before the span, so we will pass input first,
+         then span."
+      */
+      li.removeChild(span);
+      button.textContent = 'save';
+    } else if (button.textContent === 'save') {
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      /* What does the line of code above do?
+         "We can use the span to place the new input element into the DOM using insertBefore()." 
+         "We want to place the new input element first, before the span, so we will pass input first,
+         then span."
+      */
+      li.removeChild(input);
+      button.textContent = 'edit';
+    }
+  }
+});
